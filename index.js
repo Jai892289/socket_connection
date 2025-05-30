07c6a2081b204216d5f4c55beefe48b1ff45f0bd
ZetaReticuli01 <jaideepg@im24x7.com>
2025-05-30 21:59:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+
+const app = express();
+const server = createServer(app);
+const PORT=3005
+
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  },
+  transports: ['websocket', 'polling'],
+});
+
+
+io.on('connection', (socket)=>{
+    console.log("New client connected", socket.id);
+
+    socket.on('message', (message) => {
+        console.log('Received message:', message);
+        io.emit('message', "hello from server");
+    });
+})
+
+
+
+app.get('/', (req, res)=>{
+    res.send('Hello, World!');
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
